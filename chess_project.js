a8dfe72d2e24caadf131e85d1b6923c48b67af3d
Tejash <tejashsharan2022@gmail.com>
2023-08-21 23:45:27 +0530

    added some extra javascript

--- chess_project.js
+++ chess_project.js
@@ -236,7 +236,7 @@ const bishop='<div class="peice" id="bishop"><svg xmlns="http://www.w3.org/2000/
 const knight='<div class="peice" id="knight"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M96 48L82.7 61.3C70.7 73.3 64 89.5 64 106.5V238.9c0 10.7 5.3 20.7 14.2 26.6l10.6 7c14.3 9.6 32.7 10.7 48.1 3l3.2-1.6c2.6-1.3 5-2.8 7.3-4.5l49.4-37c6.6-5 15.7-5 22.3 0c10.2 7.7 9.9 23.1-.7 30.3L90.4 350C73.9 361.3 64 380 64 400H384l28.9-159c2.1-11.3 3.1-22.8 3.1-34.3V192C416 86 330 0 224 0H83.8C72.9 0 64 8.9 64 19.8c0 7.5 4.2 14.3 10.9 17.7L96 48zm24 68a20 20 0 1 1 40 0 20 20 0 1 1 -40 0zM22.6 473.4c-4.2 4.2-6.6 10-6.6 16C16 501.9 26.1 512 38.6 512H409.4c12.5 0 22.6-10.1 22.6-22.6c0-6-2.4-11.8-6.6-16L384 432H64L22.6 473.4z"/></svg></div>'
 
 const div=[]
-let body=document.querySelector("body")
+let body=document.querySelector("section")
 for(let i=0;i<64;i++){
     div[i]=document.createElement("div")
     div[i].classList.add("squares")
@@ -277,13 +277,33 @@ squares.forEach(el=>{
     })
     el.addEventListener("drop",(e)=>{
         e.stopPropagation()
-        console.log(e.target)
+        // console.log(e.target)
+        // if(e.target.parentNode.hasChildNodes()){
+        //     console.log(e.target)
+        // }
+        // else
         if(e.target.hasChildNodes()){
-            e.target.lastChild.remove()
-            e.target.appendChild(element)
-            // console.log(e.target.innerHTML)
-        }
-        else
+        e.target.parentNode.appendChild(element)
+        // e.target.FirstChild.remove()
+        e.target.parentNode.insertBefore(e.target,element)
+        console.log(e.target.parentNode)
+    }
+        else{
+        console.log(e.target)
         e.target.appendChild(element)
+        }
+        // e.target.parentNode.appendChild(element)
+        // console.log(e.target)
+        // console.log(e.target)
+        // console.log(e.target.parentNode.hasChildNodes())
+        // e.target.parentNode.appendChild(element)
     })
 })
+const newdiv=[]
+const outerBox=document.querySelector(".outerBox")
+for(let i=0;i<31;i++){
+    newdiv[i]=document.createElement("div")
+    newdiv[i].classList.add("outerSquare")
+    newdiv[i].classList.add("i")
+    outerBox.appendChild(newdiv[i])
+}
